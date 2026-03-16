8bd8fc25f378b54b11716da668da0cd8830f9a49
Matevz Morato <matevz.morato@gmail.com>
2026-03-16 12:56:46 +0100

    Set undistortion coefficients to 0 on old devices
    On old[er] devices we skip undistortion, just
    as it is skipped in StereoDepth node, as the
    calibrated coefficients are overfit.
    
    This PR matches the behavior of the StereoDepth
    node also in the fact that the tracked coeffiecnts
    when undistoriton is enables are still however
    set to 0.

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "04e486fa659ba621486ec0219deac665cbac4d60")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "999ffa6f5c7fd3d977beb601df5aa9730029c20b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
