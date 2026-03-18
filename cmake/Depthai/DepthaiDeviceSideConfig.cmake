ea0d5fd95950353ba157f335406d56321f8353c3
Matevz Morato <matevz.morato@gmail.com>
2026-03-18 11:28:01 +0100

    Bump RVC2 and RVC4 FW to develop

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "747b3781a390caf3e0e2e78a77f201b0fd3fc22a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "d3fe6a1f0078219d242611bc1e896592e650d771")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
