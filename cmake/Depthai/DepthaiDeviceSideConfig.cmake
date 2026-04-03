e0122536f8c36d8518f29c91ab837730752c337f
aljazkonec1 <53098513+aljazkonec1@users.noreply.github.com>
2026-04-03 14:50:29 +0200

    Feat: ImgTransformations track Extrinsics (#1728)
    * Add Extrinsics and utiliy functions to dai::ImgTransformations
    
    * Fix opencv compatibility
    
    * fix import
    
    * fix import issues #2
    
    * fix imports #3
    
    * Remove fmt
    
    * Bump FW
    
    * Implement suggestions and better safeguards.
    
    * Move  functions to common matrixOps.cpp file
    
    * Fix common bindings for dai::Extrinsics.
    
    * Coderabbit suggestions part 2.
    
    * Improve handling of homogeneous points and checks
    
    * Smaller refactor and simplification of code
    
    * Extrinsics tracking in Rectification node.
    
    * Better mateq handling
    
    * Better rectification and calibration handler
    
    * Improve docstrings of ImgTransformations.
    
    * Fix test crashing in case of DEPTHAI_FETCH, remove unwanted proto param.
    
    * Move Extrinsics function definitions to cpp
    
    * Bump FW.
    
    * Further improvements to docstrings
    
    * Remove getExtrinsicsToLowestSocket in favor of getCameraExtrinsics
    
    * getCameraWithLowestId is now public
    
    * Bump FW
    
    * Bump FW
    
    * Remove opencv implementations
    
    * Formatting, docstrings and smaller cleanup.
    
    * Remove unused variable
    
    * Add distortion and undistortion unit tests
    
    * Bump FW.
    
    * Set default rotation matrix.
    
    * Fix failing tests.
    
    * change Extrinsics serialization to DEPTHAI_SERIALIZE_OPTIONAL to keep backwards compatibility.
    
    * Fix deserialization issues.
    
    * Attempt Windows build fix
    
    * Imrpove docstrings and pybindings
    
    * Add call site to binding.
    
    * Bump FW
    
    * Clean diff
    
    * Bump device FW.
    
    * Bump FW.
    
    * Bump FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "d3fe6a1f0078219d242611bc1e896592e650d771")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "98ae0ad92adb3dd28e72b7ae9c7f0e990da4e58a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
