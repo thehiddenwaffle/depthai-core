82d70d0504d817ec051dd9c4aff50e4c43207c5d
stas.bucik <stas.bucik@luxonis.com>
2026-04-13 15:42:08 +0200

    Remove 60FPS tests and add 45FPS
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- tests/src/onhost_tests/multi_device_fsync_test.cpp
+++ tests/src/onhost_tests/multi_device_fsync_test.cpp
@@ -5,7 +5,8 @@
 
 TEST_CASE("Test Multi-device external frame sync with different FPS values", "[fsync]") {
     // auto fps = GENERATE(10.0f, 13.0f, 18.5f, 30.0f, 60.0f, 120.0f, 240.0f, 300.0f, 600.0f);
-    auto fps = GENERATE(10.0f, 13.0f, 18.5f, 30.0f, 60.0f);
+    // 60 FPS has a issue. STM looses sync on 60 FPS
+    auto fps = GENERATE(10.0f, 13.0f, 18.5f, 30.0f, 45.0f);
     CAPTURE(fps);
     struct TestThresholds thresholds {
         .syncThresholdSec = 1/(2*fps), // lower this limit when we have better accuracy for timestamps
