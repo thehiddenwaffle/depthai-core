5b6d60f789ebc5ad88f5ed864bc5a07f36f62374
stas.bucik <stas.bucik@luxonis.com>
2026-04-16 08:14:08 +0200

    Bump rvc4 device
    Signed-off-by: stas.bucik <stas.bucik@luxonis.com>

--- cmake/Depthai/DepthaiDeviceRVC4Config.cmake
+++ cmake/Depthai/DepthaiDeviceRVC4Config.cmake
@@ -3,4 +3,4 @@
 set(DEPTHAI_DEVICE_RVC4_MATURITY "snapshot")
 
 # "version if applicable"
-set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+ebe0d01b1f609471918fe803213e10f1799f22fb")
+set(DEPTHAI_DEVICE_RVC4_VERSION "0.0.1+7ec0df9b5b31fe11fbb1b9a4a7cc44f4ad4e24fa")
