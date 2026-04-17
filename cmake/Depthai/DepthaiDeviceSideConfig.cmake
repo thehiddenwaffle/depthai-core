c7156696d5832290c099bfcac2d356e14cac4d95
Matevz Morato <matevz.morato@gmail.com>
2026-04-17 15:33:26 +0200

    Fix LR check on RVC2

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "60f6cd094a6286b57fd8c019867be04ca199aa14")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "5d95b6ea34b52775bfd32766140fe9b7e2eeda7b")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
