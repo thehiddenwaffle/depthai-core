5ff363618e996d674d5347a113d5c5df6dff6395
Matevz Morato <matevz.morato@gmail.com>
2026-04-20 17:49:57 +0200

    Bump device side & add onInit RPC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "7751ef5630e7a4cfb1196d5c8074a497f686d74c")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c0429013e47ee9d55b3a2a2ac4238eafba29b312")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
