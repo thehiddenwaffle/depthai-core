dafe1adddcfc2822f12044e700a0d691d1131cd1
Matevz Morato <matevz.morato@gmail.com>
2026-04-21 12:32:00 +0200

    Bump device side

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "c0429013e47ee9d55b3a2a2ac4238eafba29b312")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "93d16732414d9beabd182ff6a45ca7d9f926822a")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
