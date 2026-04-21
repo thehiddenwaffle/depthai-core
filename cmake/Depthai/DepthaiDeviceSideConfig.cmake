1bcc00d1b10dc5f2f93069013b69f2f097e129c4
MaticTonin <matictonin@gmail.com>
2026-04-21 18:07:18 +0200

    Fix the Basalt examples

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "93d16732414d9beabd182ff6a45ca7d9f926822a")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "305986a801815cdec55c43db7fd442010065582d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
