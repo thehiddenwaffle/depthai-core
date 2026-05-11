84e0d56bd05157f5bf10d6c7824bb118e8f43c22
Jakob Socan <jakob.socan@luxonis.com>
2026-05-11 14:55:15 +0200

    Fixed stall on camera_output because of xlink

--- cmake/ports/xlink/portfile.cmake
+++ cmake/ports/xlink/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     HEAD_REF develop_server
     PATCHES
         no-hunter.patch
+        dispatcher-queue-and-cleanup-fixes.patch
 )
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
