1363a08956a5bc0acf16bc1a6a8f8d37254f1d78
MaticTonin <matictonin@gmail.com>
2026-05-15 14:31:34 +0200

    Add ToF RAW output

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6855587409122eefbfe4b722e2dd2cf2c70db4e9")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "c42eb95bec3d917e90eb4215a648880e8105465d")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
