65fbb85a2c097774cbd6be11e943a075ddd01673
Matevz Morato <matevz.morato@gmail.com>
2026-05-16 12:15:08 +0200

    Fixes caught by CI (OpenSSL include & xtensor utils)

--- cmake/depthaiDependencies.cmake
+++ cmake/depthaiDependencies.cmake
@@ -57,7 +57,9 @@ if(NOT CONFIG_MODE OR (CONFIG_MODE AND NOT DEPTHAI_SHARED_LIBS))
     # spdlog for library and device logging
     find_package(spdlog ${_QUIET} CONFIG REQUIRED)
 
-    find_package(OpenSSL REQUIRED)
+    if(DEPTHAI_ENABLE_CURL OR DEPTHAI_ENABLE_REMOTE_CONNECTION)
+        find_package(OpenSSL REQUIRED)
+    endif()
     # Log collection dependencies
     if(DEPTHAI_ENABLE_CURL)
         find_package(CURL ${_QUIET} CONFIG REQUIRED)
