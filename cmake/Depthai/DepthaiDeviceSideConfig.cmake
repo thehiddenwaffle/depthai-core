8a371f6f1d07d29d6c6a1a65f131dd870a2e9c27
Jakob Socan <jakob.socan@luxonis.com>
2026-05-27 13:39:44 +0200

    Bump FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "6ece083076b94d5af71acab60879f1134cf0dab0")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "9682a34b70b6f61767a7e7b2f1ab3a08a90efcca")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
