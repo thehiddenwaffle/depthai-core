30606c175f6a1ce8893e73641a8610c56555cd1c
Jakob Socan <jakob.socan@luxonis.com>
2026-05-29 15:27:34 +0200

    Made CI telemetry url more dynamic

--- tests/run_tests_entrypoint.sh
+++ tests/run_tests_entrypoint.sh
@@ -7,9 +7,6 @@ sleep 2
 # Activate Python environment
 source /workspace/venv/bin/activate
 
-export DEPTHAI_TELEMETRY_API_KEY='phc_zZmy6ywwrAmtdhXyZcy4fTmpdJxnh7QcR55XvZoZCmqz'
-export DEPTHAI_TELEMETRY_URL='https://b.luxonis.com'
-
 # Run your tests with passed arguments (e.g., rvc2 or rvc4)
 cd /workspace/tests
 echo "Running tests with args: $@"
