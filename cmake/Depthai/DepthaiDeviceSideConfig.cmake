a8b3e4afa2f587a7f6f1511fac37310cc44d55c7
Matevz Morato <matevz.morato@gmail.com>
2026-06-05 15:33:34 +0200

    Bump RVC2 with OV9728 on CAM_A override

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "dbaedf585f6af5a79c6df72f974e95cf283636b2")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "8d6a04d380ce182e0cb3a4694309426370295a9f")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
